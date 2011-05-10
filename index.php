e471db9817ff0a99f01ecf642e5905057d87ad8f
Sumardi Shukor <me@sumardi.net>
2011-05-11 03:11:38 +0800

    Modified comments.

--- index.php
+++ index.php
@@ -1,17 +1,19 @@
 <?php
-/*
-Mahua Framework is free software; you can redistribute it and/or modify it under the terms of 
-the GNU General Public License as published by the Free Software Foundation; either version 2 
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
-without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
-See the GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along with Mahua Framework. 
-If not, see <http://www.gnu.org/licenses/>
-
-Author : Sumardi Shukor <smd@php.net.my>
+/**
+ * Mahua Framework
+ * a simple & lightweight php framework
+ *
+ * @author		Sumardi Shukor <smd@php.net.my>
+ * @license		GNU GPL
+ * @version		0.1
+ * 
+ * Mahua Framework is free software; you can redistribute it and/or modify it under the terms of 
+ * the GNU General Public License as published by the Free Software Foundation; either version 2 
+ * of the License, or (at your option) any later version.
+ * 
+ * You should have received a copy of the GNU General Public License along with Mahua Framework. 
+ * If not, see <http://www.gnu.org/licenses/>
+ *
  */
  
 /*** physical path to the system ***/
